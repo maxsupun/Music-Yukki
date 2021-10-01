849a644145dc87f942fb31849fb17ce65aa3a2a1
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-02 03:42:51 +0700

    fixes

--- Yukki/Plugins/play.py
+++ Yukki/Plugins/play.py
@@ -248,7 +248,6 @@ async def play(_, message: Message):
             ) 
             return
         what = "Query Given"
-        await LOG_CHAT(message, what)
         query = message.text.split(None, 1)[1]
         mystic = await message.reply_text("🔎 **Searching...**")
         try:
@@ -276,7 +275,7 @@ async def play(_, message: Message):
         buttons = search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, duration4, duration5, user_id, query)
         hmo = await message.reply_photo(
             photo=thumb, 
-            caption=(f"1️⃣ <b>{title1[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID1})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n2️⃣ <b>{title2[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID2})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n3️⃣ <b>{title3[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID3})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n4️⃣ <b>{title4[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID4})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n5️⃣ <b>{title5[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID5})**\n└ ⚡ __Powered by Veez Music AI__</u>"),    
+            caption=(f"1️⃣ <b>{title1[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID1})**\n└ ⚡ __Powered by Veez Music AI__\n\n2️⃣ <b>{title2[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID2})**\n└ ⚡ __Powered by Veez Music AI__\n\n3️⃣ <b>{title3[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID3})**\n└ ⚡ __Powered by Veez Music AI__\n\n4️⃣ <b>{title4[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID4})**\n└ ⚡ __Powered by Veez Music AI__\n\n5️⃣ <b>{title5[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID5})**\n└ ⚡ __Powered by Veez Music AI__"),    
             reply_markup=InlineKeyboardMarkup(buttons),
         )  
         disable_web_page_preview=True
@@ -350,7 +349,7 @@ async def startyuplay(_,CallbackQuery):
     except Exception as e:
         return await CallbackQuery.message.edit(f"Error Occured\n**Possible reason could be**:{e}")
     if duration == "None":
-        return await CallbackQuery.message.reply_text(f"❌ __live stream video not supported__")      
+        return await CallbackQuery.message.reply_text(f"❌ live stream video not supported")      
     if CallbackQuery.from_user.id != int(user_id):
         return await CallbackQuery.answer("💡 sorry, this not for you", show_alert=True)
     await CallbackQuery.message.delete()
@@ -522,7 +521,7 @@ async def popat(_,CallbackQuery):
     if i == 1:
         buttons = search_markup2(ID6, ID7, ID8, ID9, ID10, duration6, duration7, duration8, duration9, duration10 ,user_id, query)
         await CallbackQuery.edit_message_text(
-            f"6️⃣ <b>{title6[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID6})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n7️⃣ <b>{title7[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID7})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n8️⃣ <b>{title8[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID8})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n9️⃣ <b>{title9[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID9})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n🔟 <b>{title10[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID10})**\n└ ⚡ __Powered by Veez Music AI__</u>",    
+            f"6️⃣ <b>{title6[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID6})**\n└ ⚡ __Powered by Veez Music AI__\n\n7️⃣ <b>{title7[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID7})**\n└ ⚡ __Powered by Veez Music AI__\n\n8️⃣ <b>{title8[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID8})**\n└ ⚡ __Powered by Veez Music AI__\n\n9️⃣ <b>{title9[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID9})**\n└ ⚡ __Powered by Veez Music AI__\n\n🔟 <b>{title10[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID10})**\n└ ⚡ __Powered by Veez Music AI__",    
             reply_markup=InlineKeyboardMarkup(buttons),
         )  
         disable_web_page_preview=True
@@ -530,7 +529,7 @@ async def popat(_,CallbackQuery):
     if i == 2:
         buttons = search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, duration4, duration5, user_id, query)
         await CallbackQuery.edit_message_text(
-            f"1️⃣ <b>{title1[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID1})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n2️⃣ <b>{title2[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID2})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n3️⃣ <b>{title3[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID3})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n4️⃣ <b>{title4[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID4})**\n└ ⚡ __Powered by Veez Music AI__</u>\n\n5️⃣ <b>{title5[:25]}</b>\n├ 💡 <u>**[More information](https://t.me/{BOT_USERNAME}?start=info_{ID5})**\n└ ⚡ __Powered by Veez Music AI__</u>",    
+            f"1️⃣ <b>{title1[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID1})**\n└ ⚡ __Powered by Veez Music AI__\n\n2️⃣ <b>{title2[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID2})**\n└ ⚡ __Powered by Veez Music AI__\n\n3️⃣ <b>{title3[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID3})**\n└ ⚡ __Powered by Veez Music AI__\n\n4️⃣ <b>{title4[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID4})**\n└ ⚡ __Powered by Veez Music AI__\n\n5️⃣ <b>{title5[:25]}</b>\n├ 💡 **[More information](https://t.me/{BOT_USERNAME}?start=info_{ID5})**\n└ ⚡ __Powered by Veez Music AI__",    
             reply_markup=InlineKeyboardMarkup(buttons),
         )  
         disable_web_page_preview=True
