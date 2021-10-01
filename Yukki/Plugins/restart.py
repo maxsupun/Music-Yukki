0c57e90ab7c835d36923b126492eda85571fb627
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-01 09:58:34 +0700

    Update restart.py

--- Yukki/Plugins/restart.py
+++ Yukki/Plugins/restart.py
@@ -10,7 +10,7 @@ from ..YukkiUtilities.tgcallsrun import (yukki, convert, download, clear, get, i
 from Yukki.YukkiUtilities.database.queue import get_active_chats
 from Yukki.YukkiUtilities.database.queue import (get_active_chats, is_active_chat, add_active_chat, remove_active_chat, music_on, is_music_playing, music_off)
 
-@app.on_message(filters.command("restartp") & filters.user(SUDOERS))
+@app.on_message(filters.command("restart") & filters.user(SUDOERS))
 async def theme_func(_, message):
     A = "downloads"
     B = "raw_files"
@@ -27,7 +27,7 @@ async def theme_func(_, message):
         pass
     for x in served_chats:
         try:
-            await app.send_message(x, "Rikudo Senin has just restarted herself. Sorry for the issues.\n\nStart playing after 10-15 seconds again.")
+            await app.send_message(x, "veez mega server has just restarted.\n\nsorry for the issues, start playing after 15-20 seconds again.")
         except Exception:
             pass
     served_chatss = []
@@ -43,7 +43,7 @@ async def theme_func(_, message):
         except Exception as e:
             await message.reply_text(f"{e}")
             pass    
-    x = await message.reply_text(f"__Restarting Rikudo Senin!__")   
+    x = await message.reply_text(f"🔄 __restarting veez mega bot...__")   
     await start_restart_stage(x.chat.id, x.message_id)
     os.execvp(f"python{str(pyver.split(' ')[0])[:3]}", [
               f"python{str(pyver.split(' ')[0])[:3]}", "-m", "Yukki"])    
