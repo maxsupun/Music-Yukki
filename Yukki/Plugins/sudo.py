5a52afe05351080a2201b56a29e8e52c6c866bc7
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-01 09:05:44 +0700

    Update sudo.py

--- Yukki/Plugins/sudo.py
+++ Yukki/Plugins/sudo.py
@@ -9,11 +9,11 @@ from Yukki.YukkiUtilities.database.sudo import (get_sudoers, get_sudoers, remove
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-@app.on_message(filters.command("addsudop") & filters.user(OWNER))
+@app.on_message(filters.command("addsudo") & filters.user(OWNER))
 async def useradd(_, message: Message):
     if not message.reply_to_message:
         if len(message.command) != 2:
-            await message.reply_text("Reply to a user's message or give username/user_id.")
+            await message.reply_text("reply to a user's message or give username/user_id")
             return
         user = message.text.split(None, 1)[1]
         if "@" in user:
@@ -22,32 +22,32 @@ async def useradd(_, message: Message):
         from_user = message.from_user 
         sudoers = await get_sudoers()
         if user.id in sudoers:
-            return await message.reply_text("Aleady a Sudo User.")
+            return await message.reply_text("✅ already a **sudo user**")
         added = await add_sudo(user.id)
         if added:
-            await message.reply_text(f"Added **{user.mention}** as a Super User for Rikudo Senin OwO")
+            await message.reply_text(f"✅ added **{user.mention}** as super sudo user")
             return os.execvp("python3", ["python3", "-m", "Yukki"])
-        await edit_or_reply(message, text="Something wrong happened, check logs.")  
+        await edit_or_reply(message, text="something wrong happened, check logs.")  
         return
     from_user_id = message.from_user.id
     user_id = message.reply_to_message.from_user.id
     mention = message.reply_to_message.from_user.mention
     sudoers = await get_sudoers()
     if user_id in sudoers:
-        return await message.reply_text("Already a Sudo User.")
+        return await message.reply_text("✅ already a **sudo user**")
     added = await add_sudo(user_id)
     if added:
-        await message.reply_text(f"Added **{mention}** as a Super User for Rikudo Senin OwO")
+        await message.reply_text(f"✅ added **{mention}** as super sudo user")
         return os.execvp("python3", ["python3", "-m", "Yukki"])
-    await edit_or_reply(message, text="Something wrong happened, check logs.")  
+    await edit_or_reply(message, text="something wrong happened, check logs.")  
     return    
           
               
-@app.on_message(filters.command("delsudop") & filters.user(OWNER))
+@app.on_message(filters.command("delsudo") & filters.user(OWNER))
 async def userdel(_, message: Message):
     if not message.reply_to_message:
         if len(message.command) != 2:
-            await message.reply_text("Reply to a user's message or give username/user_id.")
+            await message.reply_text("reply to a user's message or give username/user_id.")
             return
         user = message.text.split(None, 1)[1]
         if "@" in user:
@@ -55,29 +55,29 @@ async def userdel(_, message: Message):
         user = (await app.get_users(user))
         from_user = message.from_user      
         if user.id not in await get_sudoers():
-            return await message.reply_text(f"Not a part of Rikudo Senin's Sudo.")        
+            return await message.reply_text(f"❌ user is not a part of veez mega")        
         removed = await remove_sudo(user.id)
         if removed:
-            await message.reply_text(f"Removed **{user.mention}** from Rikudo Senin's Sudo.")
+            await message.reply_text(f"✅ removed **{user.mention}** from sudo user")
             return os.execvp("python3", ["python3", "-m", "Yukki"])
-        await message.reply_text(f"Something wrong happened.")
+        await message.reply_text(f"something wrong happened, check logs.")
         return
     from_user_id = message.from_user.id
     user_id = message.reply_to_message.from_user.id
     mention = message.reply_to_message.from_user.mention
     if user_id not in await get_sudoers():
-        return await message.reply_text(f"Not a part of Rikudo Senin's Sudo.")        
+        return await message.reply_text(f"❌ user is not a part of **veez mega**")        
     removed = await remove_sudo(user_id)
     if removed:
-        await message.reply_text(f"Removed **{mention}** from Rikudo Senin's Sudo.")
+        await message.reply_text(f"✅ removed **{mention}** from sudo user")
         return os.execvp("python3", ["python3", "-m", "Yukki"])
-    await message.reply_text(f"Something wrong happened.")
+    await message.reply_text(f"something wrong happened, check logs.")
                 
                           
-@app.on_message(filters.command("sudolistp"))
+@app.on_message(filters.command("sudolist"))
 async def sudoers_list(_, message: Message):
     sudoers = await get_sudoers()
-    text = "**__Sudo Users List of Rikudo Senin:-__**\n\n"
+    text = "💡 **sudo users list:**\n\n"
     for count, user_id in enumerate(sudoers, 1):
         try:                     
             user = await app.get_users(user_id)
@@ -86,6 +86,6 @@ async def sudoers_list(_, message: Message):
             continue                     
         text += f"➤ {user}\n"
     if not text:
-        await message.reply_text("No Sudo Users")  
+        await message.reply_text("❌ no sudo users")  
     else:
         await message.reply_text(text) 
