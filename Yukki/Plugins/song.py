92aba994e43116670b1fb2c7e588bab40efb75e1
zxce3 <memetpea25@gmail.com>
2021-10-03 05:34:47 +0700

    Close Button

--- Yukki/Plugins/song.py
+++ Yukki/Plugins/song.py
@@ -260,8 +260,11 @@ def search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, dura
             [ 
                 
                 InlineKeyboardButton(text="⬅️", callback_data=f'chonga 1|{query}|{user_id}'), 
-                InlineKeyboardButton(text="🗑 Close", callback_data=f"ppcl2 smex|{user_id}") ,
                 InlineKeyboardButton(text="➡️", callback_data=f'chonga 1|{query}|{user_id}')             
+            ],
+            [
+                InlineKeyboardButton(text="🗑 Close", callback_data=f"ppcl2 smex|{user_id}") ,
+
             ],
         ]
     return buttons   
@@ -280,8 +283,11 @@ def search_markup2(ID6, ID7, ID8, ID9, ID10, duration6, duration7, duration8, du
             [ 
                 
                 InlineKeyboardButton(text="⬅️", callback_data=f'chonga 2|{query}|{user_id}'), 
-                InlineKeyboardButton(text="🗑 Close", callback_data=f"ppcl2 smex|{user_id}") ,
                 InlineKeyboardButton(text="➡️", callback_data=f'chonga 2|{query}|{user_id}')             
+            ],
+            [
+                InlineKeyboardButton(text="🗑 Close", callback_data=f"ppcl2 smex|{user_id}") ,
+            
             ],
         ]
     return buttons     
