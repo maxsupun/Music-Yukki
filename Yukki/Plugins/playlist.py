4dba7f1c52220891068e1e374c19f642c6fa4df3
levina <82658782+levina-lab@users.noreply.github.com>
2021-11-05 16:40:54 +0700

    none

--- Yukki/Plugins/playlist.py
+++ Yukki/Plugins/playlist.py
@@ -50,7 +50,7 @@ async def pause_cmd(_, message):
 
 @Client.on_message(command(["delmyplaylist", "delmyplaylist@VeezMegaBot"]) & other_filters)
 async def delmyplaylist(_, message):
-    usage = ("usage:\n\n/delmyplaylist [numbers between 1-30] (to delete a particular music in playlist)\n\nor\n\n /delmyplaylist all (to delete whole playlist)")
+    usage = ("usage:\n\n/delmyplaylist [numbers between 1-30] (to delete a particular music in playlist)\n\n/delmyplaylist all (to delete whole playlist)")
     if len(message.command) < 2:
         return await message.reply_text(usage)
     name = message.text.split(None, 1)[1].strip()
@@ -84,7 +84,7 @@ async def delmyplaylist(_, message):
 async def delchatplaylist(_, message):
     a = await app.get_chat_member(message.chat.id , message.from_user.id)
     if not a.can_manage_voice_chats:
-        return await message.reply_text("you must be admin with permission:\n\n» ❌ __Can manage voice chat__")
+        return await message.reply_text("you must be admin with permission:\n\n» ❌ __Can manage video chat__")
     usage = ("usage:\n\n/delchatplaylist [numbers between 1-30] (to delete a particular music in playlist)\n\nor\n\n /delchatplaylist all (to delete whole playlist)")
     if len(message.command) < 2:
         return await message.reply_text(usage)
