661a7b1bbcb3eb38c6e3dadc3ecc793f540aef40
levina <82658782+levina-lab@users.noreply.github.com>
2021-11-07 15:09:35 +0700

    none

--- Yukki/Plugins/callback.py
+++ Yukki/Plugins/callback.py
@@ -312,7 +312,7 @@ async def stopvc(_,CallbackQuery):
         user_id = CallbackQuery.from_user.id
         user_name = CallbackQuery.from_user.first_name
         rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
-        await CallbackQuery.edit_message_text("✅ this music playback has ended")
+        await CallbackQuery.edit_message_text("✅ this music playback has ended", reply_markup=close_keyboard)
     else:
         await CallbackQuery.answer(f"❌ no music is currently playing", show_alert=True)
 
