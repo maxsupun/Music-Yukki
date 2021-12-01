d66e052635d85aedffd1971483c37ab15dfdd8dd
levina <82658782+levina-lab@users.noreply.github.com>
2021-12-01 15:44:55 +0700

    something

--- Yukki/Plugins/audioandvideo.py
+++ Yukki/Plugins/audioandvideo.py
@@ -71,8 +71,11 @@ async def close_admin(_, query: CallbackQuery):
 
 @Client.on_callback_query(filters.regex("cls"))
 async def close_user(_, query: CallbackQuery):
-    await query.message.delete()
-    await query.answer()
+    try:
+        await query.message.delete()
+        await query.message.reply_to_message.delete()
+    except:
+        pass
 
 
 @Client.on_callback_query(filters.regex(pattern=r"down"))
