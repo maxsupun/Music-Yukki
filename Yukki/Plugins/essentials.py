1d5c8c95da0beaffa7cbcb683671f31883b9c41b
levina <82658782+levina-lab@users.noreply.github.com>
2021-12-10 08:22:22 +0700

    update

--- Yukki/Plugins/essentials.py
+++ Yukki/Plugins/essentials.py
@@ -1,51 +1,38 @@
-from pyrogram import filters
 import os
-import subprocess
-import shutil
 import re
+import subprocess
 import sys
 import traceback
+from asyncio import create_subprocess_shell, sleep, subprocess
+from html import escape
 from inspect import getfullargspec
 from io import StringIO
 from time import time
-from Yukki.YukkiUtilities.database.queue import (is_active_chat, add_active_chat, remove_active_chat, music_on, is_music_playing, music_off)
-from Yukki.YukkiUtilities.database.onoff import (is_on_off, add_on, add_off)
-from Yukki.YukkiUtilities.database.blacklistchat import (blacklisted_chats, blacklist_chat, whitelist_chat)
-from Yukki.YukkiUtilities.database.gbanned import (get_gbans_count, is_gbanned_user, add_gban_user, add_gban_user)
-from Yukki.YukkiUtilities.database.theme import (_get_theme, get_theme, save_theme)
-from Yukki.YukkiUtilities.database.assistant import (_get_assistant, get_assistant, save_assistant)
-from ..config import DURATION_LIMIT
-from Yukki.YukkiUtilities.tgcallsrun import (yukki, clear, get, is_empty, put, task_done)
-from ..YukkiUtilities.helpers.decorators import errors
-from ..YukkiUtilities.helpers.filters import command
-from ..YukkiUtilities.helpers.gets import (get_url, themes, random_assistant)
-from ..YukkiUtilities.helpers.logger import LOG_CHAT
-from ..YukkiUtilities.helpers.thumbnails import gen_thumb
-from ..YukkiUtilities.helpers.chattitle import CHAT_TITLE
-from ..YukkiUtilities.helpers.ytdl import ytdl
-from ..YukkiUtilities.helpers.inline import (play_keyboard, search_markup)
+
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from sys import version as pyver
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from Yukki import app, SUDOERS, OWNER
-from ..YukkiUtilities.helpers.filters import command
-from ..YukkiUtilities.helpers.decorators import errors
-from Yukki.YukkiUtilities.database.functions import start_restart_stage
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import Message, ReplyKeyboardMarkup
+
+from Yukki import SUDOERS, app
+from Yukki.Utilities.tasks import add_task, rm_task
 
 
-@Client.on_message(command('update') & filters.user(OWNER))
-@errors
-async def update(_, message: Message):
-    m = subprocess.check_output(["git", "pull"]).decode("UTF-8")
-    if str(m[0]) != "A":
-        x = await message.reply_text("found updates, pulling now...")
-        await start_restart_stage(x.chat.id, x.message_id)
-        os.execvp("python3", ["python3", "-m", "Yukki"])
-    else:
-        await message.reply_text("bot is up-to-date")
-        
+m = None
+p = print
+r = None
+arrow = lambda x: (x.text if isinstance(x, Message) else "") + "\n`→`"
+
+
+async def eor(msg: Message, **kwargs):
+    func = (
+        (msg.edit_text if msg.from_user.is_self else msg.reply)
+        if msg.from_user
+        else msg.reply
+    )
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
+
+
 async def aexec(code, client, message):
     exec(
         "async def __aexec(client, message): "
@@ -54,35 +41,82 @@ async def aexec(code, client, message):
     return await locals()["__aexec"](client, message)
 
 
-async def edit_or_reply(msg: Message, **kwargs):
-    func = msg.edit_text if msg.from_user.is_self else msg.reply
-    spec = getfullargspec(func.__wrapped__).args
-    await func(**{k: v for k, v in kwargs.items() if k in spec})
+async def iter_edit(message: Message, text: str):
+    async for m in userbot.iter_history(message.chat.id):
+
+        # If no replies found, reply
+        if m.message_id == message.message_id:
+            return 0
+
+        if not m.from_user or not m.text or not m.reply_to_message:
+            continue
+
+        if (
+            (m.reply_to_message.message_id == message.message_id)
+            and (m.from_user.id == message.from_user.id)
+            and ("→" in m.text)
+        ):
+            try:
+                return await m.edit(text)
+            except MessageNotModified:
+                return
 
 
 @app.on_message(
     filters.user(SUDOERS)
     & ~filters.forwarded
     & ~filters.via_bot
-    & filters.command("eval")
+    & ~filters.edited
+    & filters.command("eval"),
 )
-async def executor(client, message):
+async def executor(client, message: Message):
+    global m, p, r
     if len(message.command) < 2:
-        return await edit_or_reply(message, text="» please give me some command to execute.")
+        return await eor(message, text="Command needed to execute")
+
     try:
         cmd = message.text.split(" ", maxsplit=1)[1]
     except IndexError:
         return await message.delete()
-    t1 = time()
+
+    if message.chat.type == "channel":
+        return
+
+    m = message
+    p = print
+
+    # To prevent keyboard input attacks
+    if m.reply_to_message:
+        r = m.reply_to_message
+        if r.reply_markup and isinstance(r.reply_markup, ReplyKeyboardMarkup):
+            return await eor(m, text="INSECURE!")
+    status = None
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = StringIO()
     redirected_error = sys.stderr = StringIO()
     stdout, stderr, exc = None, None, None
     try:
-        await aexec(cmd, client, message)
-    except Exception:
-        exc = traceback.format_exc()
+        task, task_id = await add_task(
+            aexec,
+            "Eval",
+            cmd,
+            client,
+            m,
+        )
+
+        text = f"{arrow('')} Pending Task `{task_id}`"
+        if not message.edit_date:
+            status = await m.reply(text, quote=True)
+
+        await task
+    except Exception as e:
+        e = traceback.format_exc()
+        print(e)
+        exc = e.splitlines()[-1]
+
+    await rm_task()
+
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
@@ -95,113 +129,87 @@ async def executor(client, message):
     elif stdout:
         evaluation = stdout
     else:
-        evaluation = ".:: SUCCESS ::."
-    final_output = f"**:: OUTPUT ::**\n\n```{evaluation.strip()}```"
-    if len(final_output) > 4096:
+        evaluation = "Success"
+
+    final_output = f"**→**\n`{escape(evaluation.strip())}`"
+
+    if len(final_output) > 4102:
         filename = "output.txt"
         with open(filename, "w+", encoding="utf8") as out_file:
             out_file.write(str(evaluation.strip()))
-        t2 = time()
-        keyboard = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="⏳", callback_data=f"runtime {t2-t1} Seconds"
-                    )
-                ]
-            ]
-        )
         await message.reply_document(
             document=filename,
-            caption=f"**INPUT:**\n`{cmd[0:980]}`\n\n**OUTPUT:**\n`Attached Document`",
+            caption="`→`\n  **Attached Document**",
             quote=False,
-            reply_markup=keyboard,
         )
-        await message.delete()
         os.remove(filename)
-    else:
-        t2 = time()
-        keyboard = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="⏳",
-                        callback_data=f"runtime {round(t2-t1, 3)} Seconds",
-                    )
-                ]
-            ]
-        )
-        await edit_or_reply(message, text=final_output, reply_markup=keyboard)
+        if status:
+            await status.delete()
+        return
 
-
-@app.on_callback_query(filters.regex(r"runtime"))
-async def runtime_func_cq(_, cq):
-    runtime = cq.data.split(None, 1)[1]
-    await cq.answer(runtime, show_alert=True)
+    # Edit the output if input is edited
+    if message.edit_date:
+        status_ = await iter_edit(message, final_output)
+        if status_ == 0:
+            return await message.reply(final_output, quote=True)
+        return
+    if not status.from_user:
+        status = await userbot.get_messages(status.chat.id, status.message_id)
+    await eor(status, text=final_output, quote=True)
 
 
 @app.on_message(
     filters.user(SUDOERS)
     & ~filters.forwarded
     & ~filters.via_bot
+    & ~filters.edited
     & filters.command("sh"),
 )
-async def shellrunner(client, message):
+async def shellrunner(_, message: Message):
     if len(message.command) < 2:
-        return await edit_or_reply(message, text="**usage:**\n\n» /sh git pull")
+        return await eor(message, text="**Usage:**\n/sh git pull")
+
+    if message.reply_to_message:
+        r = message.reply_to_message
+        if r.reply_markup and isinstance(
+            r.reply_markup,
+            ReplyKeyboardMarkup,
+        ):
+            return await eor(message, text="INSECURE!")
+    output = ""
     text = message.text.split(None, 1)[1]
     if "\n" in text:
         code = text.split("\n")
-        output = ""
-        for x in code:
-            shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x)
-            try:
-                process = subprocess.Popen(
-                    shell,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                )
-            except Exception as err:
-                print(err)
-                await edit_or_reply(message, text=f"**ERROR:**\n```{err}```")
-            output += f"**{code}**\n"
-            output += process.stdout.read()[:-1].decode("utf-8")
-            output += "\n"
+        shell = " ".join(code)
     else:
-        shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", text)
-        for a in range(len(shell)):
-            shell[a] = shell[a].replace('"', "")
-        try:
-            process = subprocess.Popen(
-                shell,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-        except Exception as err:
-            print(err)
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            errors = traceback.format_exception(
-                etype=exc_type,
-                value=exc_obj,
-                tb=exc_tb,
-            )
-            return await edit_or_reply(
-                message, text=f"**ERROR:**\n```{''.join(errors)}```"
-            )
-        output = process.stdout.read()[:-1].decode("utf-8")
+        shell = text
+    process = await create_subprocess_shell(
+        shell,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    out, errorz = await process.communicate()
+    if errorz:
+        error = f"**INPUT:**\n```{escape(text)}```\n\n**ERROR:**\n```{errorz.decode('utf-8')}```"
+        return await eor(message, text=error)
+    output += out.decode("utf-8")
+    output += "\n"
     if str(output) == "\n":
         output = None
     if output:
-        if len(output) > 4096:
+        if len(f"**INPUT:**\n```{escape(text)}```\n\n**OUTPUT:**\n```{(output)}```") > 4118:
             with open("output.txt", "w+") as file:
                 file.write(output)
-            await app.send_document(
-                message.chat.id,
-                "output.txt",
-                reply_to_message_id=message.message_id,
-                caption="`Output`",
+            await message.reply_document(
+                "output.txt", caption=f"{escape(text)}"
             )
             return os.remove("output.txt")
-        await edit_or_reply(message, text=f"**OUTPUT:**\n```{output}```")
+        await eor(
+            message,
+            text=f"**INPUT:**\n```{escape(text)}```\n\n**OUTPUT:**\n```{(output)}```",
+        )
     else:
-        await edit_or_reply(message, text="**OUTPUT: **\n`no output`")
+        return await eor(
+            message,
+            text=f"**INPUT:**\n```{escape(text)}```\n\n**OUTPUT: **\n`No output`",
+        )
