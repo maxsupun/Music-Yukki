7bcf6a21a20f6cd07bb081db03e6fc24f1dbe37e
levina <82658782+levina-lab@users.noreply.github.com>
2021-12-17 16:55:28 +0700

    improve

--- Yukki/Plugins/broadcast.py
+++ Yukki/Plugins/broadcast.py
@@ -6,8 +6,8 @@ from Yukki.YukkiUtilities.database.chats import (get_served_chats, is_served_cha
 from ..YukkiUtilities.helpers.filters import command
 
 
-@app.on_message(filters.command("broadcast") & filters.user(OWNER))
-async def broadcast_message(_, message):
+@app.on_message(filters.command("broadcast_pin") & filters.user(OWNER))
+async def broadcast_message_pin(_, message):
     if not message.reply_to_message:
         pass
     else :
@@ -55,4 +55,46 @@ async def broadcast_message(_, message):
             sent += 1
         except Exception:
             pass
-    await message.reply_text(f"✅ **broadcasted message in {sent} chats\n\n📌 with {pin} pins.**")
+    await message.reply_text(f"✅ **broadcasted message in {sent} chats\n📌 with {pin} pins.**")
+
+
+@app.on_message(filters.command("broadcast") & filters.user(OWNER))
+async def broadcast_message_nopin(_, message):
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await app.forward_messages(i, y, x)
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await message.reply_text(f"✅ **broadcasted message in {sent} chats")
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**usage**:\n/broadcast (message)"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await app.send_message(i, text=text)
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await message.reply_text(f"✅ **broadcasted message in {sent} chats")
