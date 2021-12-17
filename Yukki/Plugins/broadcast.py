4bc97c142780a1ab501db999b26dde1a09011a9a
levina <82658782+levina-lab@users.noreply.github.com>
2021-12-17 16:40:43 +0700

    changes

--- Yukki/Plugins/broadcast.py
+++ Yukki/Plugins/broadcast.py
@@ -1,12 +1,12 @@
 import asyncio
-from Yukki import app, SUDOERS
+from Yukki import app, OWNER
 from pyrogram import filters, Client
 from pyrogram.types import Message
 from Yukki.YukkiUtilities.database.chats import (get_served_chats, is_served_chat, add_served_chat, get_served_chats)
 from ..YukkiUtilities.helpers.filters import command
 
 
-@app.on_message(filters.command("broadcast") & filters.user(SUDOERS))
+@app.on_message(filters.command("broadcast") & filters.user(OWNER))
 async def broadcast_message(_, message):
     if not message.reply_to_message:
         pass
