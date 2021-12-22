2ee3b2d748d3046a49f9edc10e1f75b2b6ed4bdf
levina <82658782+levina-lab@users.noreply.github.com>
2021-12-22 13:38:46 +0700

    added `raw_files` cleaner

--- Yukki/Plugins/cleaners.py
+++ Yukki/Plugins/cleaners.py
@@ -1,18 +1,21 @@
-from Yukki import app, SUDOERS
+from Yukki import app, OWNER
 from pyrogram import filters, Client
 from pyrogram.types import Message
 from ..YukkiUtilities.helpers.filters import command
 import subprocess
 import shutil
 import os
-   
-    
-@Client.on_message(command("clean") & filters.user(SUDOERS))
-async def clear_storage(_, message: Message):    
-    dir = 'downloads'
-    dir1 = 'search'
-    shutil.rmtree(dir)
+
+
+@Client.on_message(command("clean") & filters.user(OWNER))
+async def clear_storage(_, message: Message):   
+    dir0 = 'downloads'
+    dir1 = 'raw_files'
+    dir2 = 'search'
+    shutil.rmtree(dir0)
     shutil.rmtree(dir1)
-    os.mkdir(dir)
+    shutil.rmtree(dir2)
+    os.mkdir(dir0)
     os.mkdir(dir1)
-    await message.reply_text("✅ cleaned all **temp** dir(s) !")
+    os.mkdir(dir2)
+    await message.reply_text("✅ Cleaned all **temp** dir(s)")
