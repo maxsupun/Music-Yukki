7a5e434783faaf33880d4731851f8f600dfd4dac
levina <82658782+levina-lab@users.noreply.github.com>
2021-12-25 23:06:00 +0700

    revert

--- Yukki/Plugins/cleaners.py
+++ Yukki/Plugins/cleaners.py
@@ -5,17 +5,14 @@ from ..YukkiUtilities.helpers.filters import command
 import subprocess
 import shutil
 import os
-
-
+   
+    
 @Client.on_message(command("clean") & filters.user(OWNER))
-async def clear_storage(_, message: Message):   
-    dir0 = 'downloads'
-    dir1 = 'raw_files'
-    dir2 = 'search'
-    shutil.rmtree(dir0)
+async def clear_storage(_, message: Message):    
+    dir = 'downloads'
+    dir1 = 'search'
+    shutil.rmtree(dir)
     shutil.rmtree(dir1)
-    shutil.rmtree(dir2)
-    os.mkdir(dir0)
+    os.mkdir(dir)
     os.mkdir(dir1)
-    os.mkdir(dir2)
-    await message.reply_text("✅ Cleaned all **temp** dir(s)")
+    await message.reply_text("✅ Cleaned all **temp** dir(s) !")
