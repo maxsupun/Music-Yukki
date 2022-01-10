5e23829085c47c4bdc95a3dab31f92f9ed812404
levina <82658782+levina-lab@users.noreply.github.com>
2022-01-10 12:16:55 +0700

    cleaning

--- Yukki/Plugins/lyrics.py
+++ Yukki/Plugins/lyrics.py
@@ -1,80 +1,94 @@
-from pyrogram.types import (
-    CallbackQuery,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    InputMediaDocument,
-    InputMediaVideo,
-    InputMediaAudio,
-    Message,
-)
-from Yukki import app
-from pyrogram import Client, filters
-from youtubesearchpython import VideosSearch
-import lyricsgenius
+import os
 import re
 
-@Client.on_callback_query(filters.regex(pattern=r"lyrics"))
-async def lyricssex(_,CallbackQuery):
+import lyricsgenius
+from Yukki import app
+from pyrogram.types import Message
+from pyrogram import Client, filters
+from youtubesearchpython import VideosSearch
+
+
+@app.on_callback_query(filters.regex(pattern=r"lyrics"))
+async def lyrics_data(_, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     try:
-        id, user_id = callback_request.split("|") 
+        id, user_id = callback_request.split("|")
     except Exception as e:
-        return await CallbackQuery.message.edit(f"an error occured\n**reason**: {e}")
-    url = (f"https://www.youtube.com/watch?v={id}")
+        return await CallbackQuery.message.edit(
+            f"error: `{e}`"
+        )
+    url = f"https://www.youtube.com/watch?v={id}"
     print(url)
     try:
         results = VideosSearch(url, limit=1)
         for result in results.result()["result"]:
-            title = (result["title"])
+            title = result["title"]
     except Exception as e:
-        return await CallbackQuery.answer("song not found, due to youtube issues.", show_alert=True)   
+        return await CallbackQuery.answer(
+            "song not found due to youtube issue", show_alert=True
+        )
     x = "OXaVabSRKQLqwpiYOn-E4Y7k3wj-TNdL5RfDPXlnXhCErbcqVvdCF-WnMR5TBctI"
     y = lyricsgenius.Genius(x)
-    print(title)
-    t = re.sub(r'[^\w]', ' ', title)
-    print(t)
+    t = re.sub(r"[^\w]", " ", title)
     y.verbose = False
     S = y.search_song(t, get_full_info=False)
     if S is None:
-        return await CallbackQuery.answer("❌ results of lyrics not found", show_alert=True)
+        return await CallbackQuery.answer(
+            "sorry lyrics not found", show_alert=True
+        )
     await CallbackQuery.message.delete()
     userid = CallbackQuery.from_user.id
     usr = f"[{CallbackQuery.from_user.first_name}](tg://user?id={userid})"
     xxx = f"""
-⚡️ __Powered by Veez Music AI__
-
-**Request By:** {usr}
 **Song Name:** __{title}__
-
-**Lyric Found:** __{S.title}__
 **Artist Name:** {S.artist}
+**Requested By:** {usr}
 
-🎧 **__Lyrics:__**
-
+**__Lyrics:__**
 {S.lyrics}"""
-    await CallbackQuery.message.reply_text(xxx)
-    
-    
-@Client.on_message(filters.command("lyrics"))
-async def lrsearch(_, message: Message):  
-    m = await message.reply_text("searching lyrics...")
+    if len(xxx) > 4096:
+        filename = "lyrics.txt"
+        with open(filename, "w+", encoding="utf8") as out_file:
+            out_file.write(str(xxx.strip()))
+        await CallbackQuery.message.reply_document(
+            document=filename,
+            caption=f"**OUTPUT:**\n\n`Lyrics`",
+            quote=False,
+        )
+        os.remove(filename)
+    else:
+        await CallbackQuery.message.reply_text(xxx)
+
+
+@app.on_message(filters.command("lyrics"))
+async def lyric_search(_, message: Message):
+    if len(message.command) < 2:
+        return await message.reply_text("**usage:**\n\n/lyrics [music name]")
+    m = await message.reply_text("🔍 Searching lyrics...")
     query = message.text.split(None, 1)[1]
     x = "OXaVabSRKQLqwpiYOn-E4Y7k3wj-TNdL5RfDPXlnXhCErbcqVvdCF-WnMR5TBctI"
     y = lyricsgenius.Genius(x)
     y.verbose = False
     S = y.search_song(query, get_full_info=False)
     if S is None:
-        return await m.edit("❌ results of lyrics not found")
+        return await m.edit("sorry lyrics not found")
     xxx = f"""
-⚡️ __Powered by Veez Music AI__
-
 **Song Name:** __{query}__
-
-**Lyric Found:** __{S.title}__
 **Artist Name:** {S.artist}
 
-🎧 **__Lyrics:__**
-
+**__Lyrics:__**
 {S.lyrics}"""
-    await m.edit(xxx)
+    if len(xxx) > 4096:
+        await m.delete()
+        filename = "lyrics.txt"
+        with open(filename, "w+", encoding="utf8") as out_file:
+            out_file.write(str(xxx.strip()))
+        await message.reply_document(
+            document=filename,
+            caption=f"**OUTPUT:**\n\n`Lyrics`",
+            quote=False,
+        )
+        os.remove(filename)
+    else:
+        await m.edit(xxx)
