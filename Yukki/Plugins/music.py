fa80f67d202a7717593df155da9fb37a1c85371d
levina <82658782+levina-lab@users.noreply.github.com>
2022-01-16 11:21:39 +0700

    removal

--- Yukki/Plugins/music.py
+++ Yukki/Plugins/music.py
@@ -73,27 +73,22 @@ async def play(_, message: Message):
         return await message.reply_text("» bot is under maintenance, sorry for the inconvenience!")
     a = await app.get_chat_member(message.chat.id , BOT_ID)
     if a.status != "administrator":
-        await message.reply_text(f"💡 To use me, I need to be an Administrator with the following permissions:\n\n» ❌ __Delete messages__\n» ❌ __Add users__\n» ❌ __Add new Admins__ \n» ❌ __Manage video chat__\n\nData is **updated** automatically after you **promote me**")
+        await message.reply_text(f"💡 To use me, I need to be an Administrator with the following permissions:\n\n» ❌ __Delete messages__\n» ❌ __Add users__\n» ❌ __Manage video chat__\n\nData is **updated** automatically after you **promote me**")
         return
     if not a.can_manage_voice_chats:
         await message.reply_text(
-        "missing required permission:"
+        "💡 To use me, Give the permission below:"
         + "\n\n» ❌ __Manage video chat__")
         return
     if not a.can_delete_messages:
         await message.reply_text(
-        "missing required permission:"
+        "💡 To use me, Give the permission below:"
         + "\n\n» ❌ __Delete messages__")
         return
     if not a.can_invite_users:
         await message.reply_text(
-        "missing required permission:"
+        "💡 To use me, Give the permission below:"
         + "\n\n» ❌ __Add users__")
-        return
-    if not a.can_promote_members:
-        await message.reply_text(
-        "missing required permission:"
-        + "\n\n» ❌ __Add new Admins__")
         return
     try:
         b = await app.get_chat_member(message.chat.id , ASSID) 
@@ -105,9 +100,6 @@ async def play(_, message: Message):
                         "https://t.me/+", "https://t.me/joinchat/"
                     )
             await ASS_ACC.join_chat(invitelink)
-            await message.chat.promote_member(
-                ASSID, can_manage_voice_chats=True
-            )
             await remove_active_chat(chat_id)
     except UserNotParticipant:
         try:
@@ -117,9 +109,6 @@ async def play(_, message: Message):
                         "https://t.me/+", "https://t.me/joinchat/"
                     )
             await ASS_ACC.join_chat(invitelink)
-            await message.chat.promote_member(
-                ASSID, can_manage_voice_chats=True
-            )
             await remove_active_chat(chat_id)
         except UserAlreadyParticipant:
             pass
