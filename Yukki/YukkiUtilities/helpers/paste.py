cc8e8a3e8f1376900326738786c5b9f912db7d6a
levina <82658782+levina-lab@users.noreply.github.com>
2022-01-18 10:26:01 +0700

    none

--- Yukki/YukkiUtilities/helpers/paste.py
+++ Yukki/YukkiUtilities/helpers/paste.py
@@ -17,7 +17,7 @@ def _netcat(host, port, content):
 
 
 async def paste(content):
-    loop = asyncio.get_running_loop()
+    loop = asyncio.get_event_loop()
     link = await loop.run_in_executor(
         None, partial(_netcat, "ezup.dev", 9999, content)
     )
