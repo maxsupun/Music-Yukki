201e40dfde3decebaf89cab680de1dcd97a6cdc8
levina <82658782+levina-lab@users.noreply.github.com>
2022-01-26 19:40:04 +0700

    none

--- Yukki/converter/converter.py
+++ Yukki/converter/converter.py
@@ -20,7 +20,7 @@ async def convert(file_path: str) -> str:
     try:
         proc = await asyncio.create_subprocess_shell(
             f"ffmpeg -y -i {file_path} -f s16le -ac 1 -ar 48000 -acodec pcm_s16le {out}",
-            asyncio.subprocess.PIPE,
+            stdin=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE,
         )
 
