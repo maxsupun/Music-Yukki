71a3b6dd1f604033ca23e191ea6976624b4ade8e
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-09 10:46:48 +0700

    none

--- Yukki/Plugins/song.py
+++ Yukki/Plugins/song.py
@@ -288,8 +288,8 @@ def search_markup2(ID6, ID7, ID8, ID9, ID10, duration6, duration7, duration8, du
 def gets(videoid, user_id):
     buttons= [
             [
-                InlineKeyboardButton(text="⬇️ Get Audio", callback_data=f'gets audio|{videoid}|{user_id}'),
-                InlineKeyboardButton(text="⬇️ Get Video", callback_data=f'gets video|{videoid}|{user_id}')
+                InlineKeyboardButton(text="📥 Get Audio", callback_data=f'gets audio|{videoid}|{user_id}'),
+                InlineKeyboardButton(text="📥 Get Video", callback_data=f'gets video|{videoid}|{user_id}')
             ],
             [
                 InlineKeyboardButton(text="🗑 Close", callback_data=f'close2')
