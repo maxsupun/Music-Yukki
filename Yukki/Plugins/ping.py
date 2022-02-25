5107c11aad92d646ba57cc43394bd210fa58a992
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-25 07:50:49 +0700

    [check] some checks

--- Yukki/Plugins/ping.py
+++ Yukki/Plugins/ping.py
@@ -7,6 +7,7 @@ import time
 from datetime import datetime
 from ..YukkiUtilities.helpers.time import get_readable_time
 
+
 async def bot_sys_stats():
     bot_uptime = int(time.time() - YUKKI_START_TIME)
     cpu = psutil.cpu_percent(interval=0.5)
@@ -22,12 +23,12 @@ async def bot_sys_stats():
 
 @app.on_message(filters.command(["ping", "ping@VeezMegaBot"]))
 async def ping(_, message):
-    uptime = await bot_sys_stats()
-    start = datetime.now()
     response = await message.reply_photo(
         photo="cache/ping.png",
-        caption=">> pong !"
+        caption=">> pinging..."
     )
+    uptime = await bot_sys_stats()
+    start = datetime.now()
     end = datetime.now()
     resp = (end - start).microseconds / 1000
-    await response.edit_text(f"🏓 `PONG!`\n⚡ `{resp}` ms\n\n🖥 System Stats:\n{uptime}")  
+    await response.edit_text(f"🏓 `PONG!`\n⚡ `{resp}` ms\n\n🖥 System Stats:\n{uptime}")
