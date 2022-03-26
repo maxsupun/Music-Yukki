82cda88390241b3dfbd2ef93696e7b1175ac3752
levina <82658782+levina-lab@users.noreply.github.com>
2022-03-27 06:21:30 +0700

    [check] just a few changes

--- Yukki/config.py
+++ Yukki/config.py
@@ -2,15 +2,17 @@ import os
 from os import getenv
 from dotenv import load_dotenv
 
-load_dotenv()
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
+load_dotenv() # environment
+
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "54000"))
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+BOT_TOKEN = getenv("BOT_TOKEN")
+SESSION_NAME = getenv("SESSION_NAME", "session")
+
+ASS_ID = int(getenv("ASS_ID"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001306851903"))
-ASS_ID = int(getenv("ASS_ID", ""))
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "54000"))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . -").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1757169682").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1757169682").split()))
