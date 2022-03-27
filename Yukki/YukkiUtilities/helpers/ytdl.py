ac348a41bf8366840e076a7b37489a503b6b4b91
levina <82658782+levina-lab@users.noreply.github.com>
2022-03-27 08:31:58 +0700

    [check] just a few changes

--- Yukki/YukkiUtilities/helpers/ytdl.py
+++ Yukki/YukkiUtilities/helpers/ytdl.py
@@ -3,12 +3,13 @@ from yt_dlp import YoutubeDL
 ytdl = YoutubeDL(
     {
         "format": "bestaudio[ext=m4a]",
-        "geo-bypass": True,
+        "geo_bypass": True,
         "noprogress": True,
         "nocheckcertificate": True,
         "outtmpl": "downloads/%(id)s.%(ext)s",
+        "no_warnings": True,
         "quite": True,
     }
 )
 
-ytdl_opts = {"format" : "bestaudio[ext=m4a]", "quiet":True}
+ytdl_opts = {"format": "bestaudio[ext=m4a]", "quiet": True}
