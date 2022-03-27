6dd8814bbe0481110fba001ca7cd99e671e840b1
levina <82658782+levina-lab@users.noreply.github.com>
2022-03-27 20:33:45 +0700

    [check] just a few changes

--- Yukki/Plugins/maintenance.py
+++ Yukki/Plugins/maintenance.py
@@ -1,43 +1,44 @@
 from Yukki import app, SUDOERS
-from pyrogram import filters, Client
-from pyrogram.types import Message
-from Yukki.YukkiUtilities.database.onoff import (is_on_off, add_on, add_off)
 from ..YukkiUtilities.helpers.filters import command
+from Yukki.YukkiUtilities.database.onoff import add_on, add_off
+
+from pyrogram.types import Message
+from pyrogram import filters, Client
 
 
 @Client.on_message(command("maintenance") & filters.user(SUDOERS))
-async def smex(_, message):
-    usage = "**usage:**\n/maintenance [enable|disable]"
+async def maintenance_off_on(_, message):
+    usage = "**usage:**\n\n/maintenance [on / off]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     chat_id = message.chat.id
     state = message.text.split(None, 1)[1].strip()
     state = state.lower()
-    if state == "enable":
+    if state == "on":
         user_id = 1
         await add_on(user_id)
-        await message.reply_text("✅ maintenance mode enabled\n\n• from now on, user can't play music after the maintenance mode is disabled.")
-    elif state == "disable":
+        await message.reply_text("✅ The maintenance mode is enabled!\n\n• From now on user can't play music until the maintenance period is over.")
+    elif state == "off":
         user_id = 1
         await add_off(user_id)
-        await message.reply_text("❌ maintenance mode disabled\n\n• from now on, user can play music again.")
+        await message.reply_text("❌ The maintenance mode is disabled!\n\n• From now on all user can play music again.")
     else:
         await message.reply_text(usage)
 
         
-@Client.on_message(command("sptest") & filters.user(SUDOERS))
-async def sls_skfs(_, message):
-    usage = "**usage:**\n/speedtest [enable|disable]"
+@Client.on_message(command("speedtest") & filters.user(SUDOERS))
+async def speedtest_off_on(_, message):
+    usage = "**usage:**\n\n/speedtest [on / off]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     chat_id = message.chat.id
     state = message.text.split(None, 1)[1].strip()
     state = state.lower()
-    if state == "enable":
+    if state == "on":
         user_id = 2
         await add_on(user_id)
         await message.reply_text("✅ **speedtest enabled**")
-    elif state == "disable":
+    elif state == "off":
         user_id = 2
         await add_off(user_id)
         await message.reply_text("❌ **speedtest disabled**")
