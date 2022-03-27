940961506184bccf67b9d075fed5c56f1dbc85dc
levina <82658782+levina-lab@users.noreply.github.com>
2022-03-27 15:01:51 +0700

    [check] just a few changes

--- Yukki/Plugins/ping.py
+++ Yukki/Plugins/ping.py
@@ -1,10 +1,12 @@
-from pyrogram import filters, Client
-from pyrogram.types import Message
-from Yukki import app, SUDOERS, YUKKI_START_TIME
 import os
-import psutil
 import time
+import psutil
+
 from datetime import datetime
+from pyrogram.types import Message
+from pyrogram import filters, Client
+
+from Yukki import app, YUKKI_START_TIME
 from ..YukkiUtilities.helpers.time import get_readable_time
 
 
@@ -14,14 +16,14 @@ async def bot_sys_stats():
     mem = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
     stats = f'''
-» uptime: {get_readable_time((bot_uptime))}
+» Uptime: {get_readable_time((bot_uptime))}
 » CPU: {cpu}%
 » RAM: {mem}%
 » DISK: {disk}%'''
     return stats
 
 
-@app.on_message(filters.command(["ping", "ping@VeezMegaBot"]))
+@app.on_message(filters.command(["ping", "server"]) & ~filters.edited)
 async def ping(_, message):
     response = await message.reply_photo(
         photo="cache/ping.png",
@@ -29,6 +31,6 @@ async def ping(_, message):
     )
     uptime = await bot_sys_stats()
     start = datetime.now()
-    end = datetime.now()
-    resp = (end - start).microseconds / 1000
-    await response.edit_text(f"🏓 `PONG!`\n⚡ `{resp}` ms\n\n🖥 System Stats:\n{uptime}")
+    stops = datetime.now()
+    resp = (stops - start).microseconds / 1000
+    await response.edit_text(f"🏓 PONG !\n⏱ `{resp}` ms\n\n🖥 System Stats:\n{uptime}")
